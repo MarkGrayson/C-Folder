bc7e2457a42ec36222e2707828bd0284c9ea76f7
MarkGrayson <Mpal7487@Gmail.com>
2017-02-06 13:19:32 -0700

    added IntNode

---
+++ IntNode.hpp
@@ -0,0 +1,14 @@
+//
+//  IntNode.hpp
+//  FirstAfternoonCPlusPlus
+//
+//  Created by Palesh, Mark on 2/6/17.
+//  Copyright © 2017 Palesh, Mark. All rights reserved.
+//
+
+#ifndef IntNode_hpp
+#define IntNode_hpp
+
+#include <stdio.h>
+
+#endif /* IntNode_hpp */
---
+++ Model/IntNode.cpp
@@ -0,0 +1,9 @@
+//
+//  IntNode.cpp
+//  FirstAfternoonCPlusPlus
+//
+//  Created by Palesh, Mark on 2/6/17.
+//  Copyright © 2017 Palesh, Mark. All rights reserved.
+//
+
+#include "IntNode.hpp"
