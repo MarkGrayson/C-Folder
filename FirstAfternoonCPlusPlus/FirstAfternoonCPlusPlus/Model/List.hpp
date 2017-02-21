30e4bea85878a1428e688c3a4f6a7d09b500efe7
MarkGrayson <Mpal7487@Gmail.com>
2017-02-21 13:54:38 -0700

    started new header

---
+++ FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/List.hpp
@@ -0,0 +1,26 @@
+//
+//  Header.h
+//  FirstAfternoonCPlusPlus
+//
+//  Created by Palesh, Mark on 2/21/17.
+//  Copyright © 2017 Palesh, Mark. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+#include <assert.h>
+
+class List
+{
+private:
+    int size;
+    Node<Type> * front;
+    
+    
+public:
+    
+    
+    
+};
+
+#endif /* Header_h */
