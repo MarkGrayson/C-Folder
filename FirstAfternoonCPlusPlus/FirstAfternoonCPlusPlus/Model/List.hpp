dfa4f921fd6b32ca7306110c7b8346fb74060971
MarkGrayson <Mpal7487@Gmail.com>
2017-02-23 12:53:21 -0700

    updated code

--- FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/List.hpp
+++ FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/List.hpp
@@ -14,10 +14,26 @@ class List
 {
 private:
     int size;
-    Node<Type> * front;
+    List<Type> * front;
     
     
 public:
+    List<Type() : constructor
+    List<Type>()(const List<Type> & source) : copy constructor
+    List<Type>() : destructor
+    
+    addAtIndex(int index, Type value) : void
+    add(Type value) : void
+    remove(int index) : void
+    setAtIndex(int index, Type data) : Type
+    getFromIndex(int index) : Type
+    contains(Type data) : bool
+    getSize() const: int
+    getFront() const : Node<Type> *
+    getEnd() const : Node<Type> *
+    
+    
+    
     
     
     
