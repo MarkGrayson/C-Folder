e60bb9622a060229274cfc141e7ec7eb286bdcf4
MarkGrayson <Mpal7487@Gmail.com>
2017-02-23 13:23:49 -0700

    updated code

--- FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/List.hpp
+++ FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/List.hpp
@@ -18,25 +18,60 @@ private:
     
     
 public:
-    List<Type() : constructor
-    List<Type>()(const List<Type> & source) : copy constructor
-    List<Type>() : destructor
-    
-    addAtIndex(int index, Type value) : void
-    add(Type value) : void
-    remove(int index) : void
-    setAtIndex(int index, Type data) : Type
-    getFromIndex(int index) : Type
-    contains(Type data) : bool
-    getSize() const: int
-    getFront() const : Node<Type> *
-    getEnd() const : Node<Type> *
-    
-    
-    
-    
-    
+    List();
+    List(int size);
+    List<Type>();
+    List<Type>(const List<Type & toBeCopied);
+    ~List<Type>();
+    void setAtIndex(int index, Type value);
+    void addAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    int getSize();
     
 };
+
+template <class Type>
+Node<Type> * List<Type> :: getEnd() const
+{ }
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    if(size == 0)
+    {
+        Node<Type> * first = new Node<Type>(value);
+        this->front = first;
+        this->end = first;
+    }
+    else
+    {
+        Node<Type> * newFirst = new Node<Type>();
+        newFirst->setNodeData(value);
+        newFirst->setNodePointer(front);
+        //or
+        //Node<Type> * newFirst = new Node<Type>(value, front);
+        this->front = newFirst;
+    }
+    
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addEnd(Type data)
+{
+    Node<Type> * added = new Node<Type>(data);
+    if(size == 0)
+    {
+        this->front = added;
+        this->end = added;
+    }
+    else
+    {
+        end->setNodePointer(added);
+        this->end = added;
+    }
+    size++;
+
+}
 
 #endif /* Header_h */
