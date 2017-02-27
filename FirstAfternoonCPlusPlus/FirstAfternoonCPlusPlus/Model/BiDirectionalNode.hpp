4e7d027de03481e911d41e00eab3c658ae69509e
MarkGrayson <Mpal7487@Gmail.com>
2017-02-27 13:50:29 -0700

    Added BiDirectionalNode header

---
+++ FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/BiDirectionalNode.hpp
@@ -0,0 +1,39 @@
+//
+//  BiDirectionalNode.hpp
+//  FirstAfternoonCPlusPlus
+//
+//  Created by Palesh, Mark on 2/27/17.
+//  Copyright © 2017 Palesh, Mark. All rights reserved.
+//
+
+#ifndef BiDirectionalNode_h
+#define BiDirectionalNode_h
+
+template <class Type>
+class BiDirectionalNode : public Node<Type>
+{
+private:
+    BiDirectionalNode<Type> * previous;
+    BiDirectionalNode<Type> * next;
+    Type data;
+public:
+    BiDirectionalNode();
+    BiDirectionalNode(Type data);
+    BiDirectionalNode(Type data, BiDirectionalNode<Type> * previous,\
+        BiDirectionalNode<Type> * next);
+    
+}
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode() : Node()
+{
+    
+}
+
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode() : Node<Type>(data)
+{
+    
+}
+
+
+#endif /* BiDirectionalNode_h */
