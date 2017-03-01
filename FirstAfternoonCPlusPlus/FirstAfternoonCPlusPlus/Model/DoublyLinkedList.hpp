8cbb889053585684237b2d20cf2688e3bd2050bf
MarkGrayson <Mpal7487@Gmail.com>
2017-03-01 12:52:31 -0700

    Added Doubly linked class

---
+++ FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/DoublyLinkedList.hpp
@@ -0,0 +1,20 @@
+//
+//  DoublyLinkedList.hpp
+//  FirstAfternoonCPlusPlus
+//
+//  Created by Palesh, Mark on 3/1/17.
+//  Copyright © 2017 Palesh, Mark. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+#endif /* DoublyLinkedList_h */
