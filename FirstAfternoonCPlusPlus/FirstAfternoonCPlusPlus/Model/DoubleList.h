da67f4b82ac9d5bba4d91114e48431c8e16d4c5c
MarkGrayson <Mpal7487@Gmail.com>
2017-03-17 12:32:29 -0600

    Still updating code added addAtIndexFast

--- FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/DoubleList.h
+++ FirstAfternoonCPlusPlus/FirstAfternoonCPlusPlus/Model/DoubleList.h
@@ -21,7 +21,7 @@ public:
     void add(Type data);
     Type remove(int index);
     void addAtIndexFast(int index, Type value);
-    void addAtIindex(int index, Type value);
+    void addAtIndex(int index, Type value);
     Type getFromIndex(int index);
     Type getFromIndexFast(int index);
 };
@@ -54,6 +54,39 @@ void DoubleList<Type> :: add(Type value)
     this->setSize(this->getSize() + 1);
 }
 
+template<class Type>
+void DoubleList<Type> :: addAtIndex(int index, Type value)
+{
+    
+}
+
+template <class Type>
+void DoubleList<Type> :: addAtIndexFast(int index,Type value)
+{
+    asset(index >= 0 && index < this->getSize());
+    Type valueAtIndex;
+    BiDirectionalNode<Type> * reference;
+    if(index < this->getSize() / 2)
+    {
+        reference = this->getFront();
+        for(int position = 0; position < index; position++)
+        {
+            reference = reference->getNextPointer();
+        }
+    }
+    else
+    {
+        reference = this->getend();
+        for(int position = this->getSize() - 1; position < index; position--)
+        {
+            reference = reference->getPreviousPointer();
+        }
+    }
+    
+    valueAtIndex = reference->getNodeData();
+    return valueAtIndex;
+}
+
 template <class Type>
 Type DoubleList<Type> :: remove(int index)
 {
@@ -78,22 +111,19 @@ Type DoubleList<Type> :: remove(int index)
 }
 
 template <class Type>
-Type DoubleList<Type> :: getAtIndex(int index, Type data)
+Type DoubleList<Type> :: getFromIndex(int index)
 {
-    assert(index >= 0 && index < getsize);
-    Type removeData;
+    assert(index >= 0 && index < this->getSize());
+    Type valueAtIndex;
     
-    BiDirectionalNode<Type> * current = getfront;
-    
-    for (int spot = 0; spot < index; spot++)
+    BiDirectionalNode<Type> * reference = this->getFront();
+    for (int position = 0; position < index; position++)
     {
-        current = current->getNextPointer();
+        reference = reference->getNextPointer();
     }
+    valueAtIndex = reference->getNodeData();
     
-    removeData = current->getNodeData();
-    current->setNodeData(data);
-    
-    return removeData;
+    return valueAtIndex;
 }
 
 template <class Type>
